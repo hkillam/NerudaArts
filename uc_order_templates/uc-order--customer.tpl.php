5a5b34be113524e47ba830f2bf7f94b9f1060c50
hkillam <heather.killam.kw@gmail.com>
2016-02-19 23:38:31 -0500

    Customized for Neruda
    Neruda needs to put the product details in the form, so that ticket
    types (adult, youth, senior) are displayed.

--- uc_order_templates/uc-order--customer.tpl.php
+++ uc_order_templates/uc-order--customer.tpl.php
@@ -3,28 +3,6 @@
 /**
  * @file
  * This file is the default customer invoice template for Ubercart.
- *
- * Available variables:
- * - $products: An array of product objects in the order, with the following
- *   members:
- *   - title: The product title.
- *   - model: The product SKU.
- *   - qty: The quantity ordered.
- *   - total_price: The formatted total price for the quantity ordered.
- *   - individual_price: If quantity is more than 1, the formatted product
- *     price of a single item.
- *   - details: Any extra details about the product, such as attributes.
- * - $line_items: An array of line item arrays attached to the order, each with
- *   the following keys:
- *   - line_item_id: The type of line item (subtotal, shipping, etc.).
- *   - title: The line item display title.
- *   - formatted_amount: The formatted amount of the line item.
- * - $shippable: TRUE if the order is shippable.
- *
- * Tokens: All site, store and order tokens are also available as
- * variables, such as $site_logo, $store_name and $order_first_name.
- *
- * Display options:
  * - $op: 'view', 'print', 'checkout-mail' or 'admin-mail', depending on
  *   which variant of the invoice is being rendered.
  * - $business_header: TRUE if the invoice header should be displayed.
